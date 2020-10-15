3e3efb765fe89531342e96d79b2ece09b0a78b7d
JDavid80 <jdcalderong@gmail.com>
2020-10-15 16:21:23 -0500

    Prueba hostname terminada

--- js/script.js
+++ js/script.js
@@ -2,7 +2,3 @@ function mostrarMenu() {
     var nav = document.getElementById("menu-principal");
     nav.classList.toggle('mostrar');
 }
-function mostrarHostName() {
-    var Dominio = window.location.hostname;
-    alert(Dominio);
-}
